d4763a1228b4720e2228458e9a6e856cf13d1c6c
12serg34 <12serg34@mail.ru>
2019-01-17 23:39:29 +0300

    create grid of the panels that revert their color by click

--- src/main/java/gui/MainForm.java
+++ src/main/java/gui/MainForm.java
@@ -4,10 +4,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 public class MainForm {
     private JPanel field;
-    private boolean value;
+    private JPanel[][] cells;
+    private Map<JPanel, Boolean> values;
 
     public static void main(String[] args) {
         MainForm mainForm = new MainForm();
@@ -21,13 +25,35 @@ public class MainForm {
     }
 
     private void init(){
-        field.setBackground(getColor(value));
-        field.addMouseListener(new MouseAdapter() {
+
+        MouseAdapter mouseAdapter = new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                field.setBackground(getColor(value = !value));
+                JPanel panel = (JPanel)e.getComponent();
+                boolean revertValue = !values.get(panel);
+                values.put(panel, revertValue);
+                panel.setBackground(getColor(revertValue));
             }
-        });
+        };
+
+        int width = 5;
+        int height = 5;
+        values = new HashMap<>();
+        field.setLayout(new GridLayout(height, width));
+        cells = new JPanel[height][width];
+        Random random = new Random();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                JPanel panel = new JPanel();
+                panel.addMouseListener(mouseAdapter);
+                boolean value = random.nextBoolean();
+                values.put(panel, value);
+                panel.setBackground(getColor(value));
+                field.add(panel);
+                cells[i][j] = panel;
+            }
+        }
+
     }
 
     private static Color getColor(boolean value){
